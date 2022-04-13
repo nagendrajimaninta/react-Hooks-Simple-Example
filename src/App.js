8b6606577cdb7cc33f0de4994e868bf623889349
nagendrajimaninta <jimanintanagendra01@gmail.com>
2022-04-13 22:13:33 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React,{useState} from "react";
 import "./style.css";
+function UseState(){
+  return (
+    <>
+    <h1>UseState Hook allows us to track state of our functional component</h1>
+    <h2>UseState returns two things One is Initial state and the function to update the state</h2>
+    </>
+  );
+}
+function SetState(){
+  const [col,setCol] = useState("Red");
+  return (
+    <>
+    <UseState />
+    <h1>{col}</h1>
+    <button onClick={()=>setCol("Black")}>Black</button>
+    <button onClick={()=>setCol("Blue")}>Blue</button>
+    <button onClick={()=>setCol("Yellow")}>Yellow</button>
+    <button onClick={()=>setCol("Green")}>Green</button>
+    <button onClick={()=>setCol("White")}>White</button>
+    </>
+  );
+}
 function Hooks(){
   return(
     <>
+    <SetState />
+    <h2>You must import Hooks from React</h2>
     <h3>Hooks were added to react is 16.8 versiom in place of class component life cycle methods</h3>
     <h3>Hooks allows to functional component to have state and other react features </h3>
     <h1>Genarally class components are no longer needed</h1>
